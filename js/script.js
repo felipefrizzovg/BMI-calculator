bce8095a6ee38d57adfbf157354b8380d89084ce
Felipe Frizzo <felipefrizzovg@gmail.com>
2023-12-28 14:27:10 -0300

    Modularized Modal

--- js/script.js
+++ js/script.js
@@ -1,20 +1,9 @@
+import { Modal } from './modal.js'
+
 // variables
 const form = document.querySelector("form");
 const inputWeight = document.querySelector("#weight");
 const inputHeight = document.querySelector("#height");
-
-const Modal = {
-  wrapper: document.querySelector(".modal-wrapper"),
-  message: document.querySelector(".modal .title span"),
-  btnClose: document.querySelector(".close"),
-
-  open() {
-    Modal.wrapper.classList.add("open")
-  },
-  close() {
-    Modal.wrapper.classList.remove("open")
-  }
-}
 
 // creating and assigning a function to an event
 form.onsubmit = (e) => {
@@ -29,9 +18,6 @@ form.onsubmit = (e) => {
   Modal.message.innerText = message
   Modal.open()
 };
-
-Modal.btnClose.onclick = () => Modal.close()
-
 
 function BMI(weight, height) {
   return (weight / ((height / 100) ** 2)).toFixed(2);
